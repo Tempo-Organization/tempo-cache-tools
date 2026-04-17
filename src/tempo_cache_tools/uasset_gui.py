59558d63789c52400d16b0b8362cbaa909a82d71
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-17 00:59:36 -0400

    Create uasset_gui.py

---
+++ src/tempo_cache_tools/uasset_gui.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+from tempo_cache import cache
+
+
+# test this later (because tool name is not the same as the repo name)
+@dataclass
+class UassetGuiToolInfo(cache.ToolInfo):
+    tool_name: str = "uasset_gui"
+    repo_name: str = "UAssetGUI"
+    repo_owner: str = "atenfyr"
+
+
+    def get_executable_name(self) -> str:
+        if cache.is_windows():
+            return 'UAssetGUI.exe'
+        else:
+            raise ValueError('unsupported os')
+
+
+    def get_file_to_download(self) -> str:
+        if cache.is_windows():
+            return 'UAssetGUI.exe'
+        else:
+            raise ValueError('unsupported os')
