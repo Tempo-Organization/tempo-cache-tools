3fc60488271defa482a2e1c8ab9e6a1f686d0347
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-17 00:59:31 -0400

    Update fmodel.py

--- src/tempo_cache_tools/fmodel.py
+++ src/tempo_cache_tools/fmodel.py
@@ -10,15 +10,19 @@ class PatternsleuthToolInfo(cache.ToolInfo):
     repo_owner: str = "4sval"
 
 
-    def get_executable_name(self) -> str:
-        if cache.is_windows():
-            return 'FModel.exe'
-        else:
-            raise ValueError('unsupported os')
-
-
     def get_file_to_download(self) -> str:
         if cache.is_windows():
-            return 'Fmodel.zip'
+            return f'{self.tool_name}-x86_64-pc-windows-msvc.zip'
+        elif cache.is_linux():
+            return f'{self.tool_name}-x86_64-unknown-linux-gnu.tar.xz'
         else:
-            raise ValueError('unsupported os')
+            raise ValueError('Unsupported OS')
+
+
+    def get_executable_name(self) -> str:
+        if cache.is_windows():
+            return f'{self.tool_name}.exe'
+        elif cache.is_linux():
+            return f'{self.tool_name}'
+        else:
+            raise ValueError('Unsupported OS')
