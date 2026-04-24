eaf26212d69b6bc7a7620f808b96837a0ec19c2e
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2026-04-23 22:05:25 -0400

    Update uasset_gui.py

--- src/tempo_cache_tools/uasset_gui.py
+++ src/tempo_cache_tools/uasset_gui.py
@@ -1,14 +1,15 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from pathlib import Path
 
 from tempo_cache import cache
 
 
-# test this later (because tool name is not the same as the repo name)
 @dataclass
 class UassetGuiToolInfo(cache.ToolInfo):
     tool_name: str = "uasset_gui"
     repo_name: str = "UAssetGUI"
     repo_owner: str = "atenfyr"
+    file_paths: list[Path] = field(default_factory=lambda: [Path('UAssetGUI.exe')])
 
 
     def get_executable_name(self) -> str:
